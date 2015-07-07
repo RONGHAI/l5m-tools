9e89af1b85b22c28be3fba13ed532073ae25778e
Ronghai Wei <ronghai.wei@outlook.com>
2015-07-07 17:25:00 -0400

    Use `svn delete` instead of original file delete

--- lib/l5m-tools/application.rb
+++ lib/l5m-tools/application.rb
@@ -3,10 +3,21 @@ require 'pathname'
 
 module L5MTools    
     TEMPLATE_DIR = Pathname.new(__FILE__).parent + 'template'    
-    
+    puts TEMPLATE_DIR
     class Application
         require 'l5m-tools/tools'
-        include Tools        
+        include Tools
+
+        #that is svn remove
+        def rm(file, options)
+            #FileUtils.rm(file, :force => true)
+            # remove file from svn
+            package = file[(WORKSPACE.length+1)..(-1)]
+            svnfile = package[(package.index(/[\/\\]/) + 1)..(-1)]
+            package = package[0...(package.index(/[\/\\]/))]
+            require 'l5m-tools/l5m-tools-module'
+            L5MTools.svn.delete([package], {}, [ "--force", svnfile])
+        end       
         def duplicate_app(*args)
             delete = args.delete('-d')
             replacements  = {args[1] => args[2]}
@@ -20,7 +31,7 @@ module L5MTools
                         if line.length > 0 && line[0] != '#'
                             #duplicate_and_replace( line.chomp ,  replacements ) 
                             #FileUtils.rm(line, :force => true) if delete
-                            FileUtils.rm(line, :force => true) if duplicate_and_replace( line.chomp ,  replacements )[1] != line && delete
+                            rm(line, :force => true) if duplicate_and_replace( line.chomp ,  replacements )[1] != line && delete
                         end 
                     end
                 end
@@ -37,19 +48,33 @@ module L5MTools
                 #puts line #File.basename(line, ".*") 
                 unless(bn[application.length] =~ /[a-z0-9]/)
                     #puts line #File.basename(line, ".*") 
-                    FileUtils.rm(line, :force => true) if duplicate_and_replace( line.chomp ,  replacements )[1] != line && delete
+                    rm(line, :force => true) if duplicate_and_replace( line.chomp ,  replacements )[1] != line && delete
                 end                
             }
         end
 
+        def del_wo_csv(*args)
+            package = args[0]
+            application = args[1]
+            Dir.glob("#{WORKSPACE}/#{package}/{src,WebContent}/**/#{application}*.*", File::FNM_CASEFOLD).each{|line|  
+                bn = File.basename(line, ".*")
+                unless(bn[application.length] =~ /[a-z0-9]/)
+                    puts line
+                    rm(line, :force => true)
+                end                
+            }
+        end
         #read file list from file and delete them
         def del(*args)
-            replacements  = {args[1] => args[2]}
-            File.open(args[0], "r") do |infile|
-                while (line = infile.gets)
-                    line = line.chomp.strip
-                    if line.length > 0 && line[0] != '#'
-                        FileUtils.rm(line, :force => true)
+             unless File.exist?(args[0])
+                del_wo_csv(*args)
+            else
+                File.open(args[0], "r") do |infile|
+                    while (line = infile.gets)
+                        line = line.chomp.strip
+                        if line.length > 0 && line[0] != '#'
+                            rm(line, :force => true)
+                        end
                     end
                 end
             end
